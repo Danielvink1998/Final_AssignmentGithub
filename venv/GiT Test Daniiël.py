bb60f2b9ae2c5590b2b6e08eec53d25c973e57c2
danielvink <daniel.vink@student.hu.nl>
2018-09-21 14:14:01 +0200

    Deze versie bevat geen bugs

---
+++ venv/GiT Test Daniiël.py
@@ -0,0 +1,7 @@
+print('hoi')
+uurloon= eval(input('wat is je uurloon?'))
+gewerkt = eval(input('Hoeveel uur heb je gewerkt?'))
+
+salaris = (gewerkt)*(uurloon)
+print(gewerkt, 'uur werken levert', salaris,'euro op')
+
